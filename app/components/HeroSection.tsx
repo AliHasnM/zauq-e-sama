de00eac186b609115d793e6783dedd9b78f1e65a
AliHasnM <mughal201burewala@gmail.com>
2025-07-15 15:22:26 +0500

    Update HeroSection

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -32,24 +32,4 @@ const HeroSection = () => {
     )
 }
 
-export default HeroSection
-// import Link from 'next/link'
-// import React from 'react'
-// import { Spotlight } from './ui/Spotlight'
-
-// const HeroSection = () => {
-//     return (
-//         <div className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'>
-//             <Spotlight className='-top-4 left-0 md:left-60 md:-top-20' fill='white' />
-//             <div className='p-4 relative z-10 w-full text-center'>
-//                 <h1 className='mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400'>Master the art of Zauq-E-Sama</h1>
-//                 <p className='mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto'>Dive into comprehesive Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos libero iure illo ea fugiat non consectetur ipsum voluptatibus eveniet molestias. Exercitationem magni autem necessitatibus distinctio neque quis, iure sunt placeat.</p>
-//                 <div className='mt-4'>
-//                     <Link href="/courses">Explore Course</Link>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default HeroSection
+export default HeroSection;
